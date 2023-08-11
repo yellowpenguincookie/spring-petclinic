2ccb73ca0848cc177315a1e2b9fb0ee2575e8c9f
yellowpenguincookie <131837332+yellowpenguincookie@users.noreply.github.com>
2023-08-11 15:16:19 +0900

    Update run_process.sh

--- scripts/run_process.sh
+++ scripts/run_process.sh
@@ -2,7 +2,7 @@
 
 REGION="ap-northeast-2"
 ECR_REPOSITORY="257307634175.dkr.ecr.ap-northeast-2.amazonaws.com"
-ECR_DOCKER_IMAGE="${ECR_REPOSITORY}/aws00-spring-petclinic"
+ECR_DOCKER_IMAGE="${ECR_REPOSITORY}/project04-spring-petclinic"
 ECR_DOCKER_TAG="1.0"
 
 aws ecr get-login-password --region ${REGION} \
